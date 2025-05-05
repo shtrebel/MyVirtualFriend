77bbbcf0782a4d4f04a82260ae49c735e7d6ca05
aasht <a.ashtrebel@mail.ru>
2025-05-05 19:18:28 +0700

    Создан метод для случайного выбора ответов с заглушкой

--- src/Friend.java
+++ src/Friend.java
@@ -1,7 +1,15 @@
+import java.util.Random;
+
 public class Friend {
+
+    Random random = new Random();
 
     public Friend(){
 
     }
 
+    public String getRandomResponse(String[] responses) {
+        //todo добавить полноценную реализацию getRandomResponse
+        return "A";
+    }
 }
