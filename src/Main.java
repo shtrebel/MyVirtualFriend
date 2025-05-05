75e181adaf88651c1dd1c05ffddd6f8172a8998c
aasht <a.ashtrebel@mail.ru>
2025-05-06 01:07:56 +0700

    Реализована главная функция

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
-
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        Friend friend = new Friend();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(friend.respondToGreeting());
+
+        while (true) {
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.contains("пока") || input.contains("выход")) {
+                System.out.println(friend.respondToFarewell());
+                break;
+            } else if (input.contains("погод")) {
+                System.out.println(friend.respondToWeather());
+            } else if (input.contains("спорт") || input.contains("футбол")) {
+                System.out.println(friend.respondToSport());
+            } else {
+                System.out.println("Не понял. Спроси о погоде или спорте!");
+            }
+        }
 
     }
 }
